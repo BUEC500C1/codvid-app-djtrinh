218dbc54e90d60435506fa13d6e2daff40be66b3
Danny Trinh <djtrinh@bu.edu>
2020-04-13 04:32:18 -0400

    Added step 3

---
+++ maps/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+	    <MapView
+	style={ styles.map }
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
+    );
+  }
+}
